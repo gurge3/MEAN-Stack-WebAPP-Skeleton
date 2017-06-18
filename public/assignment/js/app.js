a566abe60de33542875b321aae565fcdaf068238
Xingyao Wu <gurge3@163.com>
2017-06-17 21:07:35 -0400

    MLab Connection

--- public/assignment/js/app.js
+++ public/assignment/js/app.js
@@ -1,7 +1,21 @@
 module.exports = function(app) {
     var mongoose = require('mongoose');
     mongoose.Promise = require('q').Promise;
-    mongoose.connect('mongodb://127.0.0.1/wu-xingyao-webdev');
+
+
+    var username = "admin";
+    var password = "admin";
+
+    var connectionString = 'mongodb://'+ username + ':' + password + '@ds143221.mlab.com:43221/heroku_dgt90mmz';
+
+    if(process.env.MLAB_USERNAME) {
+        connectionString = process.env.MLAB_USERNAME + ":" +
+            process.env.MLAB_PASSWORD + "@" +
+            process.env.MLAB_HOST + ':' +
+            process.env.MLAB_PORT + '/' +
+            process.env.MLAB_APP_NAME;
+    }
+    mongoose.connect(connectionString);
 
     require("./services/page.service.server.js")(app);
     require("./services/website.service.server.js")(app);
